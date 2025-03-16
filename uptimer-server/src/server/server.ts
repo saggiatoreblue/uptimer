fa89c59b06ce281b6389d516293ee709481b7841
saggiatoreblue <saggiatoreblue@gmail.com>
2025-03-16 09:02:23 -0400

    setup basic http/apollo server and express app

--- uptimer-server/src/server/server.ts
+++ uptimer-server/src/server/server.ts
@@ -1,7 +1,29 @@
-import { Express, Request, Response, NextFunction } from "express";
-import { PORT } from "./config";
+import {
+  Express,
+  Request,
+  Response,
+  NextFunction,
+  json,
+  urlencoded,
+} from "express";
+import {
+  CLIENT_URL,
+  NODE_ENV,
+  PORT,
+  SECRET_KEY_ONE,
+  SECRET_KEY_TWO,
+} from "./config";
 import http from "http";
+import cors from "cors";
 import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { ApolloServerPluginLandingPageDisabled } from "@apollo/server/plugin/disabled";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+import cookieSession from "cookie-session";
+
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { expressMiddleware } from "@apollo/server/express4";
+import logger from "./logger";
 
 const typeDefs = /* GraphQL */ `
   type User {
@@ -28,9 +50,23 @@ export default class MonitorServer {
   constructor(app: Express) {
     this.app = app;
     this.httpServer = new http.Server(app);
+    const schema = makeExecutableSchema({ typeDefs, resolvers });
+    this.server = new ApolloServer({
+      schema,
+      introspection: NODE_ENV !== "production",
+      plugins: [
+        ApolloServerPluginDrainHttpServer({ httpServer: this.httpServer }),
+        NODE_ENV === "production"
+          ? ApolloServerPluginLandingPageDisabled()
+          : ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+      ],
+    });
   }
 
   async start(): Promise<void> {
+    //Note that you must call the start() method on the ApolloServer instance
+    // before passing the instance to expressMiddleware
+    await this.server.start();
     this.standardMiddleWare(this.app);
     this.startServer();
   }
@@ -41,17 +77,53 @@ export default class MonitorServer {
       res.header("Cache-Control", "no-cache, no-store, must-revalidate");
       next();
     });
+    app.use(
+      cookieSession({
+        name: "session",
+        keys: [SECRET_KEY_ONE, SECRET_KEY_TWO],
+        maxAge: 24 * 7 * 3600000,
+        secure: NODE_ENV !== "development",
+        ...(NODE_ENV !== "development" && {
+          sameSite: "none",
+        }),
+      })
+    );
+    this.graphqlRoute(app);
+    this.healthRoute(app);
+  }
+
+  private graphqlRoute(app: Express): void {
+    app.use(
+      "/graphql",
+      cors({
+        origin: CLIENT_URL,
+        credentials: true,
+      }),
+      json({ limit: "200mb" }),
+      urlencoded({ extended: true, limit: "200mb" }),
+      expressMiddleware(this.server, {
+        context: async ({ req, res }: { req: Request; res: Response }) => {
+          return { req, res };
+        },
+      })
+    );
+  }
+
+  private healthRoute(app: Express): void {
+    app.get("/health", (_req: Request, res: Response) => {
+      res.status(200).send("Uptimer monitor service is healthy and OK.");
+    });
   }
 
   private async startServer(): Promise<void> {
     try {
       const SERVER_PORT: number = parseInt(PORT!, 10) || 5000;
       this.httpServer.listen(SERVER_PORT, () => {
-        console.info(`Server running on port ${SERVER_PORT}`);
+        logger.info(`Server running on port ${SERVER_PORT}`);
       });
-      console.info(`Server has started with process id ${process.pid}`);
+      logger.info(`Server has started with process id ${process.pid}`);
     } catch (error) {
-      console.error("error", "startServer() error " + error);
+      logger.error("error", "startServer() error " + error);
     }
   }
 }
